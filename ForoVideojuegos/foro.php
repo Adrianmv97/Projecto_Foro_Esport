332c38e65a8702291abc41dac84dfe11e2e05696
Ramon <ramon.botella.ciria@gmail.com>
2019-04-04 11:34:42 +0200

    subida de diseños iniciales del proyecto

---
+++ ForoVideojuegos/foro.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css" type="text/css">
+    <script src="main.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+<body>
+<nav class="navbar navbar-light bg-primary">
+    <div class="collapse navbar-collapse d-flex flex-row bd-highlight" id="navbarText" id="datos">
+        <span class="navbar-text">
+            Datos de usuario
+        </span>
+    </div>
+    <div class="collapse navbar-collapse d-flex flex-row-reverse bd-highlight" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+            <div class="p-2 border bg-success">
+                <li class="nav-item active">
+                    <a class="nav-link" href="cerrarSesion.php">Cerrar Sesión</a>
+                </li>
+            </div>
+        </ul>
+        <ul class="navbar-nav">
+            <div class="p-2 border bg-success">
+                <li class="nav-item active">
+                    <a class="nav-link" href="iniciarSesion.php">Iniciar Sesión</a>
+                </li>
+            </div>
+        </ul>
+    </div>
+</nav>
+<?php 
+    include('./templates/nav.php');
+?>
+<div class="jumbotron jumbotron-fluid mx-auto">
+    <h3>Tema</h3>
+    <div class="card">
+        <a href="subforo.php" class="btn btn-primary btn-lg btn-block" role="button" aria-pressed="true">Subforo 1</a>
+        <a href="subforo.php" class="btn btn-primary btn-lg btn-block" role="button" aria-pressed="true">Subforo 2</a>
+        <a href="subforo.php" class="btn btn-primary btn-lg btn-block" role="button" aria-pressed="true">Subforo 3</a>
+    </div>
+</div>
+<?php
+    include('./templates/footer.php');
+?>
+</body>
+</html>
---
+++ ForoVideojuegos/templates/ficha.php
@@ -0,0 +1,80 @@
+<div class="d-flex flex-wrap p-2 bd-highlight">
+    <div class="card text-center" style="width: 25rem;">
+        <div class="card-header">
+            Ficha 1
+        </div>
+        <div class="card-body" >
+            <h5 class="card-title">Call Of Duty</h5>
+            <p class="card-text">Niños rata everywhere</p>
+            <a href="post.php" class="btn btn-primary">Ir al post</a>
+        </div>
+        <div class="card-footer text-muted">
+            6 hours left
+        </div>
+    </div>
+    <div class="card text-center" style="width: 25rem;">
+        <div class="card-header">
+            Ficha 2
+        </div>
+        <div class="card-body" >
+            <h5 class="card-title">Maincra</h5>
+            <p class="card-text">Niños rata everywhere que fingen construir</p>
+            <a href="post.php" class="btn btn-primary">Ir al post</a>
+        </div>
+        <div class="card-footer text-muted">
+            5 hours left
+        </div>
+    </div>
+    <div class="card text-center" style="width: 25rem;">
+        <div class="card-header">
+            Ficha 3
+        </div>
+        <div class="card-body" >
+            <h5 class="card-title">Fornait</h5>
+            <p class="card-text">Niños rata everywhere que fingen construir y son unos putos camperos</p>
+            <a href="post.php" class="btn btn-primary">Ir al post</a>
+        </div>
+        <div class="card-footer text-muted">
+            4 hours left
+        </div>
+    </div>
+    <div class="card text-center" style="width: 25rem;">
+        <div class="card-header">
+            Ficha 4
+        </div>
+        <div class="card-body" >
+            <h5 class="card-title">No mans sky</h5>
+            <p class="card-text">Salio mal el videojuego, pero tienen la poca esperanza de arreglarlo</p>
+            <a href="post.php" class="btn btn-primary">Ir al post</a>
+        </div>
+        <div class="card-footer text-muted">
+            3 hours left
+        </div>
+    </div>
+    <div class="card text-center" style="width: 25rem;">
+        <div class="card-header">
+            Ficha 5
+        </div>
+        <div class="card-body" >
+            <h5 class="card-title">Habbo Hotel</h5>
+            <p class="card-text">Mitico donde los haya, se usa para ligar</p>
+            <a href="post.php" class="btn btn-primary">Ir al post</a>
+        </div>
+        <div class="card-footer text-muted">
+            2 hours left
+        </div>
+    </div>
+    <div class="card text-center" style="width: 25rem;">
+        <div class="card-header">
+            Ficha 6
+        </div>
+        <div class="card-body" >
+            <h5 class="card-title">Half Life 3</h5>
+            <p class="card-text">Algun dia...</p>
+            <a href="post.php" class="btn btn-primary">Ir al post</a>
+        </div>
+        <div class="card-footer text-muted">
+            1 hour left
+        </div>
+    </div>
+</div>
