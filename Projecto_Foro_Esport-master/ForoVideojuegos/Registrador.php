9b7ad1f26c375e3a2721483a181a915137b36479
victorgcsj <39703087+victorgcsj@users.noreply.github.com>
2019-05-15 12:56:22 +0200

    Visualizacion del post pagina subForo

---
+++ Projecto_Foro_Esport-master/ForoVideojuegos/Registrador.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="css/Inicio.css" rel="stylesheet" type="text/css"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title></title>
+
+    </head>
+    <body  class="logica">
+        <div  class="centro">
+            <?php
+            $newUser = $_REQUEST["newUser"];
+            $newPassword = $_REQUEST["newPass"];
+
+            $newUser = strtolower($newUser);
+
+            $conn = mysqli_connect('localhost', 'root', '', 'foroesport', '3306');
+            if (!$conn) {
+                die('Could not connect to MySQL: ' . mysqli_connect_error());
+            }
+            $accion = "INSERT INTO `usuarios` (`USER`, `PASSWORD`, `LEVELUSER`)"
+                    . " VALUES ('$newUser', '$newPassword' , 1)";
+
+            $accion2 = "SELECT user FROM usuarios WHERE user = '$newUser'";
+
+            $result = $conn->query($accion2);
+            $count = mysqli_num_rows($result);
+            if ($count == 1) {
+                echo "Usuario ya existente";
+                echo "<br>";
+                echo "<a href='registro.html'>Volver</a>";
+            } else {
+                if ($conn->query($accion) === TRUE) {
+                    echo "Usuario creado correctamente";
+                    echo ("<br>");
+                    echo ("<a href='IniciarSesion.html'>Iniciar Sesion</a>");
+                } else {
+                    echo "Error: " . $accion . "<br>" . $conn->error;
+                }
+            }
+
+
+            mysqli_close($conn);
+            ?>
+        </div>
+
+    </body>
+</html>
---
+++ Projecto_Foro_Esport-master/ForoVideojuegos/foro.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<?php
+session_start();
+
+if (isset($_REQUEST["accion"])) {
+    $accion = $_REQUEST["accion"];
+}
+if (isset($_REQUEST["idTemaUnico"])) {
+    $idTemaUnico = $_REQUEST["idTemaUnico"];
+}
+?>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Foro E-Sports By Ramon, Adrian & Victor</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="./css/style.css" type="text/css">
+        <script src="main.js"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <nav class="navbar navbar-light bg-primary">
+            <div class="collapse navbar-collapse d-flex flex-row bd-highlight" id="navbarText" id="datos">
+                <span class="navbar-text">
+                    Datos de usuario
+                </span>
+            </div>
+            <div class="collapse navbar-collapse d-flex flex-row-reverse bd-highlight" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <div class="p-2 border bg-success">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="registro.html">Registrarse</a>
+                        </li>
+                    </div>
+                </ul>
+                <ul class="navbar-nav">
+                    <div class="p-2 border bg-success">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="iniciarSesion.html">Iniciar Sesion</a>
+                        </li>
+                    </div>
+                </ul>
+            </div>
+        </nav>
+        <?php
+        include('./templates/nav.php');
+
+        require_once 'config.php';
+        require_once 'model.php';
+
+        $conexion = new model(Config::$host, Config::$user, Config::$pass, Config::$baseDatos);
+        if (isset($accion)) {
+            if ($accion == "ver") {
+                $tema = $conexion->verTema($idTemaUnico);
+                foreach ($tema as $valor) {
+                    echo '<div class="jumbotron jumbotron-fluid mx-auto">';
+                    echo "<h3>" . $valor['TituloTema'] . "</h3>";
+                    $foro = $conexion->verSubForos($valor['id']);
+                    foreach ($foro as $valor) {
+                        echo '<div class="card">';
+                        echo "<a href='subforo.php' class='btn btn-primary btn-lg btn-block' role='button' aria-pressed='true'>" . $valor['TituloSubForo'] . "</a>";
+                        echo '</div>';
+                    }
+                    echo '</div>';
+                }
+            }
+        } else {
+            $temas = $conexion->verTemas();
+            foreach ($temas as $valor) {
+                echo '<div class="jumbotron jumbotron-fluid mx-auto">';
+                echo "<h3>" . $valor['TituloTema'] . "</h3>";
+                $foro = $conexion->verSubForos($valor['id']);
+                foreach ($foro as $valor) {
+                    echo '<div class="card">';
+                    echo "<a href='subforo.php' class='btn btn-primary btn-lg btn-block' role='button' aria-pressed='true'>" . $valor['TituloSubForo'] . "</a>";
+                    echo '</div>';
+                }
+                echo '</div>';
+            }
+        }
+
+        include('./templates/footer.php');
+        $conexion->desconectar();
+        ?>
+    </body>
+</html>
